0948756c3a9a87d9dfe48d5225194e5f5ac06afd
Rabin Mainali <rabinmainali1999@gmail.com>
2025-02-05 11:17:13 +0545

    added custom generateToken functionality

---
+++ identity-service/src/utils/generateToken.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken")
+const crypto = require('crypto')
+const RefreshToken = require('../models/refreshToken')
+
+
+const generateToken = async(user) => {
+    const accessToken = jwt.sign({id:user._id,username:user.username,email:user.email},
+        process.env.JWT_SECRET,{expiresIn:'60m'}
+    )
+
+    const refreshToken = crypto.randomBytes(40).toString('hex')
+    const expiresAt = new Date()
+    expiresAt.setDate(expiresAt.getDate()+7)
+
+    await RefreshToken.create({token:refreshToken,user:user._id,expiresAt})
+
+    return {accessToken,refreshToken}
+}
+
+module.exports = generateToken
