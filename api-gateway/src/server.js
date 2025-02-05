fe13cd342596dd8cd2af323ddb47b1b1fbf3ae9d
Rabin Mainali <rabinmainali1999@gmail.com>
2025-02-05 12:36:14 +0545

    added middlewares and a defined proxy options for api-gateway

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,2 +1,76 @@
 require('dotenv').config()
 const express = require('express')
+const logger = require('./utils/logger')
+const cors = require('cors')
+const Redis = require('ioredis')
+const helmet = require('helmet')
+const {rateLimit} = require('express-rate-limit')
+const {RedisStore} = require('rate-limit-redis')
+const proxy = require('express-http-proxy')
+const errorHandler = require('./middlewares/errorHandler')
+
+const app = express()
+
+const redisClient = new Redis()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+//rate limiter
+const rateLimiter =  rateLimit({
+    windowMs:60*1000*5, //5 min
+    limit:200,
+    legacyHeaders:false,
+    standardHeaders:true,
+    handler:(req,res)=>{
+        logger.warn(`Sensitive rate limit exceeded for this ip ${req.ip}`)
+        res.status(429).send('Too Many Requests');
+    },
+    store: new RedisStore({
+		sendCommand: (...args) => redisClient.call(...args),
+	}),
+})
+
+app.use(rateLimiter)
+
+app.use((req,res,next)=>{
+    logger.info(`Request ${req.method} to path ${req.path}`)
+    logger.info(`Request Body -- ${req.body}`)
+    console.log(req.body)
+    next()
+})
+
+const proxyOptions = {
+    proxyReqPathResolver:(req)=>{
+        return req.originalUrl.replace(/^\/v1/,'/api')
+    },
+    proxyErrorHandler: function(err, res, next) {
+        logger.error(`proxy error : ${err.message}`)
+        next(err);
+      }
+}
+
+app.use('/v1/auth',proxy(process.env.IDENTITY_SERVICE_URL,{
+    ...proxyOptions,
+    //modify the proxy req header
+    proxyReqOptDecorator:(proxyReqOpts, srcReq)=>{
+        proxyReqOpts.headers["Content-Type"] = 'application/json'
+        return proxyReqOpts
+    },
+    userResDecorator:(proxyRes,proxyResData,userReq,userRes)=>{
+        logger.info(`Response received from identity-service : ${proxyRes.statusCode}`)
+        return proxyResData
+    }
+}))
+
+app.use(errorHandler)
+
+app.listen(process.env.PORT,()=>{
+    logger.info(`Api gateway server running on the port ${process.env.PORT}`)
+})
+
+//unhandle rejection
+process.on('unhandledRejection',(reason,promise)=>{
+    logger.error(`Unhandled Rejection due to promise ${promise} and reason ${reason}`)
+})
