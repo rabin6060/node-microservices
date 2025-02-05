4a78e563aaf05db5ec6517ab1443364b7a82c0e3
Rabin Mainali <rabinmainali1999@gmail.com>
2025-02-05 11:16:44 +0545

    added validation for user registration

--- identity-service/src/utils/user.validation.js
+++ identity-service/src/utils/user.validation.js
@@ -5,7 +5,7 @@ const validateRegistration = (data)=>{
     const userSchema = Joi.object({
         username:Joi.string().min(5).max(10).required(),
         email:Joi.string().email().required(),
-        password:Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        password:Joi.string().required(),
     })
 
     return userSchema.validate(data)
